12d3212f5153fc35d464f7b0675df18b342d6cc7
Petar Zarkov <46219231+petarzarkov@users.noreply.github.com>
2022-03-08 14:06:51 +0200

    try-eop

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ cd dist
 
 git init
 git checkout -b main
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
+# git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+# git config --local user.name "github-actions[bot]"
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/petarzarkov/nicotine-sugar.git main:gh-pages
+git push -f git@github.com:petarzarkov/nicotine-sugar.git main:gh-pages
 
 
 cd -
