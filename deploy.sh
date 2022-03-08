417e71a41db9b9b6e92482716ec02518d0448dd8
Petar Zarkov <46219231+petarzarkov@users.noreply.github.com>
2022-03-08 14:01:29 +0200

    try-agaiaain

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
 git init
 git checkout -b main
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
 git add -A
 git commit -m 'deploy'
 
