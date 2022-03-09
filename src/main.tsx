cb3c35e75d46561ab26179ea1843fde59ffe4c75
Petar Zarkov <46219231+petarzarkov@users.noreply.github.com>
2022-03-09 12:53:34 +0200

    add-context-hooks-contracts-store-theme-utils

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { SugarProvider } from "./context";
 
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SugarProvider>
+      <App />
+    </SugarProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles")
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@contracts": path.resolve(__dirname, "./src/contracts"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     }
   },
   plugins: [react()]
